34de93bc76b7b1c91eff6526355af9f4d7012b80
Okan Topdemir <149389691+OkanT6@users.noreply.github.com>
2023-12-31 22:42:35 +0300

    Add files via upload
    In this version of my codes. I'm learning to useState and useEffect.
    In the next commin, I will learn how to fetch a data from a json server by using "get" request.

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
-    
-    const [name, setName] = useState("Okan");
-    const [age,setAge]=useState(22)
 
-    const handleClick= () => {
-        setName("Berke");
-        setAge(21);
-    }
-   
-    
-   
-    return ( 
-        <div className="Home">
-                <h2>Homepage</h2>   
-                <button onClick={handleClick}>Click me</button>
-                <h2>{name} is {age} years old.</h2>
-                
-                
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-                
-        </div>
-        
-     );
+  const [name,setName]=useState("Okan")
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => (blog.id !== id))
+    setBlogs(newBlogs);
+
+  }
+
+
+  useEffect(() => {
+    console.log("useEffect() ran")
+    console.log(name)
+  },[name])
+
+  return (
+    <div className="Home">
+
+      <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs!"></BlogList>
+      <BlogList blogs={blogs.filter((blog) => (blog.author === "mario"))} title="Mario's Blogs" handleDelete={handleDelete}></BlogList>
+      <h2>{name}</h2>
+      <button onClick={()=>setName("Berke")}>Change name</button>
+      <button onClick={()=> setName("Okan")}>change again</button>
+
+    </div>
+
+  );
+
+  /* 
+  return (
+ <div className="home">
+   {blogs.map(blog => (
+     <div className="blog-preview" key={blog.id} >
+       <h2>{ blog.title }</h2>
+       <p>Written by { blog.author }</p>
+     </div>
+   }))
+ </div>
+);
+  */
 }
- 
+
 export default Home;
